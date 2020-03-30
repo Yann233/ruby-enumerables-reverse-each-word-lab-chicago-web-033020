d0a290836720c3974731beaaa37673979a126a7c
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-30 10:36:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-
+#study for .collect=>https://www.geeksforgeeks.org/ruby-array-collect-operation/
 def reverse_each_word(array)
 array=array.split(" ")
 new_array = array.collect{|word| word.reverse}
