b1883095ce2d197407e5d89ad46ffa7fee5b6234
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-03-30 10:35:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-=begin
+
 def reverse_each_word(array)
 array=array.split(" ")
 new_array = array.collect{|word| word.reverse}
 new_array.join(" ")
 end
-=end
-def reverse_each_word(array)
-  array.split(" ").collect {|word| word.reverse}.join(" ")
-end
+
+#def reverse_each_word(array)
+#  array.split(" ").collect {|word| word.reverse}.join(" ")
+#end
